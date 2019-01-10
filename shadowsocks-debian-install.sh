d6411bc6d14a3f0cee33a38e63134d72cdf9151c
EequMCC <EequMCC@163.com>
2019-01-10 23:50:16 +0800

    Update shadowsocks-debian-install.sh

--- shadowsocks-debian-install.sh
+++ shadowsocks-debian-install.sh
@@ -20,6 +20,7 @@ stop(){
 json(){
     if [ -e /etc/shadowsocks.json ];then
         del /etc/shadowsocks.json
+        rm /etc/shadowsocks.json
     fi
     echo "{">>/etc/shadowsocks.json
     echo "\"server\":\"0.0.0.0\",">>/etc/shadowsocks.json
