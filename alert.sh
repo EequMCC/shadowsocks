7e4a6f3e9ec86722c0ac5e2e00f06fdfe321df0b
EequMCC <EequMCC@163.com>
2019-11-14 21:10:06 +0800

    Update alert.sh

--- alert.sh
+++ alert.sh
@@ -2,4 +2,3 @@
 rm /usr/local/lib/python2.7/dist-packages/shadowsocks/crypto/openssl.py
 wget https://raw.githubusercontent.com/EequMCC/shadowsocks/master/openssl.py
 mv openssl.py /usr/local/lib/python2.7/dist-packages/shadowsocks/crypto
-wget https://raw.githubusercontent.com/EequMCC/shadowsocks/master/shadowsocks && chmod +x shadowsocks && mv shadowsocks /usr/bin
